18f41c9d18382c1a702f00a9b7cd622a5534a9c0
sandeep sala <sandeepsala@outlook.com>
2021-05-20 09:32:13 +0530

    Started Find Second Largest

---
+++ DS/Find_Second_Largest.py
@@ -0,0 +1,6 @@
+try: 
+    a,b,c = int(input()),int(input()),int(input())
+    find_second_largest = lambda a,b,c : sorted([a,b,c])[-1]
+    print(  find_second_largest(a,b,c)  )
+except Exception as e:
+    print(e)
