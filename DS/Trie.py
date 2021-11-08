136e29152b38a44c44a515c90cc6fa5883704538
sandeep-sala-cognoai <76934426+sandeep-sala-cognoai@users.noreply.github.com>
2021-11-08 12:07:32 +0530

    DS

--- DS/Trie.py
+++ DS/Trie.py
@@ -1,66 +1,92 @@
-from types import coroutine
+"""
+A prefix tree/Trie has three main operations:
+    1. Inserting a word into the tree.
+    2. Searching for a node that matches a given string exactly.
+    3. Returning a list of all nodes (if any) that begin with a given string prefix.
+"""
 
 
-class TriNode:
-    def __init__(self) -> None:
-        self.children = {}
-        self.endOfSting = False
+class TrieNode:
+    def __init__(self, text=''):
+        self.text = text
+        self.children = dict()
+        self.is_word = False
 
-class Trie:
-    def __init__(self) -> None:
-        self.root = TriNode()
 
-    def insertString(self, word):
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
         current = self.root
-        for i in word:
-            ch = i
-            node = current.children.get(ch)
-            if not node:
-                node = TriNode()
-                current.children.update({ch:node})
-            current = node
-        current.endOfSting = True
+        for i, char in enumerate(word):
+            if char not in current.children:
+                prefix = word[0:i+1]
+                current.children[char] = TrieNode(prefix)
+            current = current.children[char]
+        current.is_word = True
 
-    def searchString(self, word):
-        currentNode = self.root
-        for i in word:
-            node = currentNode.children.get(i)
-            if not node:
+
+    def find(self, word):
+        '''
+        Returns the TrieNode representing the given word if it exists
+        and None otherwise.
+        '''
+        current = self.root
+        for char in word:
+            if char not in current.children:
                 return False
-            currentNode = node
-        return currentNode.endOfSting
+            current = current.children[char]
+        return current.is_word
 
-def deleteString(root, word, index):
-    ch = word[index]
-    currentNode = root.children.get(ch)
-    canThisNodeBeDeleted = False
-
-    if len(currentNode.children) > 1:
-        deleteString(currentNode, word,index)
-        return False
-
-    if index == len(word) -1:
-        if len(currentNode.children) >= 1:
-            currentNode.endOfSting = False
-            return False
-        else:
-            root.children.pop(ch)
-            return True
-    
-    if currentNode.endOfSting == True:
-        deleteString(currentNode,word,index+1)
-        return False
-
-    canThisNodeBeDeleted =  deleteString(currentNode, word, index+1)
-    if canThisNodeBeDeleted == True:
-        root.children.pop(ch)
-        return True
-    else:
-        return False
+    def __child_words_for(self, node, words):
+        '''
+        Private helper function. Cycles through all children
+        of node recursively, adding them to words if they
+        constitute whole words (as opposed to merely prefixes).
+        '''
+        if node.is_word:
+            words.append(node.text)
+        for letter in node.children:
+            self.__child_words_for(node.children[letter], words)
 
 
-nTrie = Trie()
-nTrie.insertString("App")
-nTrie.insertString("Appl")
-deleteString(nTrie.root, "App", 0)
-print(nTrie.searchString("App"))
+    def starts_with(self, prefix):
+        '''
+        Returns a list of all words beginning with the given prefix, or
+        an empty list if no words begin with that prefix.
+        '''
+        words = list()
+        current = self.root
+        for char in prefix:
+            if char not in current.children:
+                return list()
+            current = current.children[char]
+
+        self.__child_words_for(current, words)
+        return words
+
+    def size(self, current = None):
+        '''
+        Returns the size of this prefix tree, defined
+        as the total number of nodes in the tree.
+        '''
+        # By default, get the size of the whole trie, starting at the root
+        if not current:
+            current = self.root
+        count = 1
+        for letter in current.children:
+            count += self.size(current.children[letter])
+        return count
+
+if __name__ == '__main__':
+    trie = PrefixTree()
+    trie.insert('apple')
+    trie.insert('app')
+    trie.insert('aposematic')
+    trie.insert('appreciate')
+    trie.insert('book')
+    trie.insert('bad')
+    trie.insert('bear')
+    trie.insert('bat')
+    print(trie.starts_with('app'))
