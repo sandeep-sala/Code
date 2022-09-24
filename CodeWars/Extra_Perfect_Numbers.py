b2d4c0eb002cfb043cac509a6e0879e839738ea9
Sandeep Sala <sandeepsala5596@gmail.com>
2022-09-24 13:58:02 +0530

    24-09-22

---
+++ CodeWars/Extra_Perfect_Numbers.py
@@ -0,0 +1,9 @@
+def extra_perfect(n):
+    return [i for i in range(1,n+1) if i%2!=0]
+
+
+print(extra_perfect(3), [1,3])
+print(extra_perfect(5), [1,3,5])
+print(extra_perfect(7), [1,3,5,7])
+print(extra_perfect(28), [1,3,5,7,9,11,13,15,17,19,21,23,25,27])
+print(extra_perfect(39), [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39])
