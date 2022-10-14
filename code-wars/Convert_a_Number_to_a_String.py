2982072ab8e27eba2d9888a8bf3303a40259175e
Sandeep Sala <sandeepsala5596@gmail.com>
2022-10-14 07:14:23 +0530

    13-10-22

---
+++ code-wars/Convert_a_Number_to_a_String.py
@@ -0,0 +1,9 @@
+
+def number_to_string(num):
+    return str(num)
+
+print(number_to_string(67), '67')
+print(number_to_string(79585), '79585')
+print(number_to_string(79585), "79585")
+print(number_to_string(1+2), '3')
+print(number_to_string(1-2), '-1')
