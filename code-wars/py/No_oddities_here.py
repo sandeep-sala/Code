bbf0c93158ac9ba7379688d7f617d476700b5dd9
Sandeep Sala <sandeepsala5596@gmail.com>
2023-01-04 07:04:11 +0530

    04-01-23

---
+++ code-wars/py/No_oddities_here.py
@@ -0,0 +1,11 @@
+def no_odds(values):
+    return [v for v in values if v % 2 == 0]
+
+
+print(no_odds([0, 1]), [0], "Zero through one")
+print(no_odds([0, 1, 2, 3]), [0, 2], "Zero through three")
+print(no_odds([1, 3, 5, 7, 9]), [], "Odds through ten")
+print(no_odds([0, 2, 4, 6, 8, 10]), [0, 2, 4, 6, 8, 10], "Evens through ten")
+print(no_odds([-1, -3, -5, -7, -9]), [], "Negative odds")
+print(no_odds([2, 4, 8, 6, 0]), [2, 4, 8, 6, 0], "Out of order")
+print(no_odds([]), [], "Empty list")
