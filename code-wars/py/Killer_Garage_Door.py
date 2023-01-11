9f3605a361f022afe9df7dc2c5af285ff5b593f7
Sandeep Sala <sandeepsala5596@gmail.com>
2023-01-11 07:17:00 +0530

    11-01-23

---
+++ code-wars/py/Killer_Garage_Door.py
@@ -0,0 +1,9 @@
+def controller(events):
+    pass
+
+
+print(controller('..........'), '0000000000')
+print(controller('P....'), '12345')
+print(controller('P.P..'), '12222')
+print(controller('..P...O...'), '0012343210')
+
