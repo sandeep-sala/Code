d18dbcb259c9d4ce8863edea302d4c1d49242527
Sandeep Sala <sandeepsala5596@gmail.com>
2023-07-08 22:18:14 +0530

    08-07-23

---
+++ code-wars/py/Reverse_List_Order.py
@@ -0,0 +1,8 @@
+def reverse_list(l):
+    return l[::-1]
+
+
+print(reverse_list([1,2,3,4]), [4,3,2,1])
+print(reverse_list([3,1,5,4]), [4,5,1,3])
+print(reverse_list([3,6,9,2]), [2,9,6,3])
+print(reverse_list([1]), [1])
