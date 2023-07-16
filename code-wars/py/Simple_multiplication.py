321a70dc53a9123e162f4c4d860410ef68c36134
Sandeep Sala <sandeepsala5596@gmail.com>
2023-07-16 20:00:25 +0530

    16-07-23

---
+++ code-wars/py/Simple_multiplication.py
@@ -0,0 +1,8 @@
+def simple_multiplication(n):
+    return n*8 if n%2==0 else n*9
+
+print(simple_multiplication(2), 16)
+print(simple_multiplication(1), 9)
+print(simple_multiplication(8), 64)
+print(simple_multiplication(4), 32)
+print(simple_multiplication(5), 45)
