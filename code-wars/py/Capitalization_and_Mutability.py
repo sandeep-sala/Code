b4fdba36a1428531ce0adbebd547b7f7ebb9d016
Sandeep Sala <sandeepsala5596@gmail.com>
2023-07-17 08:49:50 +0530

    17-07-23

---
+++ code-wars/py/Capitalization_and_Mutability.py
@@ -0,0 +1,7 @@
+def capitalize_word(word):
+    return word.capitalize()
+
+
+print(capitalize_word('word'), 'Word')
+print(capitalize_word('i'), 'I')
+print(capitalize_word('glasswear'), 'Glasswear')
